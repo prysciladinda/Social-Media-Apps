1f6f8ec4a1ef7285aa789cee4a35ac0aa489cdf5
Agung Cahya <agungcahya122@gmail.com>
2023-01-18 12:18:12 +0700

    feat: remake buttonRegister   , build : swal animation

---
+++ src/utils/swal.ts
@@ -0,0 +1,14 @@
+import Swal from "sweetalert2";
+
+export default Swal.mixin({
+  reverseButtons: true,
+  confirmButtonColor: "#00715C",
+  showCancelButton: true,
+  confirmButtonText: "#ffff",
+  showClass: {
+    popup: "animate__animated animate__fadeIn animate__faster",
+  },
+  hideClass: {
+    popup: "animate__animated animate__fadeOut animate__faster",
+  },
+});
