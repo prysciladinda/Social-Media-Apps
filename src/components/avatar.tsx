cfa535001e3a031a52eb6a1dfd9323eb251d9551
pryscila dinda <pryscila.dinda06@gmail.com>
2023-01-19 23:11:14 +0700

    fix png2

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,10 +1,10 @@
-import Sidenav from "../assets/sideNav.png";
+import Sidenav from "../assets/sideNav.jpg";
 import SideNav from "./sideNav";
 
 const Avatar = () => {
   return (
     <div className="flex gap-2 py-3 px-1  items-center">
-      <img className="w-9 h-9 rounded-full" src={Sidenav} alt="sideNav.png" />
+      <img className="w-9 h-9 rounded-full" src={Sidenav} alt="sideNav.jpg" />
       <ul className="list-none">
         <li className="text-[12px] font-bold">Amanda Emma</li>
         <li className="text-zinc-500 text-[12px] text-start">@amandaemma</li>
